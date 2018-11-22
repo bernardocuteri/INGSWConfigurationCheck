e1e596b8d1da4c5d43f9969e9a828be5ce57807a
Bernardo Cuteri <bernardo.cuteri@gmail.com>
2018-11-22 12:36:09 +0100

    Configuration check source

---
+++ ConfigurationCheck/src/main/java/it/unical/ingsw2018/ConfigurationCheck/Example.java
@@ -0,0 +1,7 @@
+package it.unical.ingsw2018.ConfigurationCheck;
+
+public class Example {
+	public int example() {
+		return 0;
+	}
+}
---
+++ ConfigurationCheck/src/test/java/it/unical/ingsw2018/ConfigurationCheck/ExampleTest.java
@@ -0,0 +1,13 @@
+package it.unical.ingsw2018.ConfigurationCheck;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    
+	@Test
+	public void testExample() {
+		assertEquals(0, new Example().example());
+	}
+}
